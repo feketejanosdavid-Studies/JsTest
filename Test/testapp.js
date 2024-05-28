84ada9b2aedd8e9feb5d60cd57a57025932b1918
feketejanosdavid <janosdavidfekete@gmail.com>
2024-05-28 10:50:45 +0200

    feladat kész

---
+++ Test/testapp.js
@@ -0,0 +1,16 @@
+// const puppeteer = require('puppeteer');
+import puppeteer from "puppeteer";
+
+(async () => {
+    console.log('Loading');
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+    await page.type('#side','30');
+    await page.type('#alpha','35');
+    await page.click('#calcButton');
+    const actual = await page.$eval('#perimeter', elem => elem.value);
+    console.log('Eredmény', actual);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await browser.close();
+})()
